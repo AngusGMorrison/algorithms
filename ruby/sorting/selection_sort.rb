3617f226fc77028df0ff3ff155af45a47329828c
Angus Morrison <contact@angus-morrison.com>
2020-02-08 18:24:54 +0000

    Selection sort

---
+++ ruby/sorting/selection_sort.rb
@@ -0,0 +1,21 @@
+=begin
+  Insertion sort
+  - O(n^2)
+  - Counterpart of bubble sort
+  - Performs poorly in real-world applications
+=end
+
+def insertion_sort(arr)
+  length = arr.length()
+  for i in (0...(length - 1)) do
+    for j in ((i + 1)...length) do
+      if arr[j] < arr[i]
+        arr[j], arr[i] = arr[i], arr[j]
+      end
+    end
+  end
+end
+
+numbers = [ 4, 2, 7, 3, 5, 6, 0 ]
+insertion_sort(numbers)
+puts numbers
