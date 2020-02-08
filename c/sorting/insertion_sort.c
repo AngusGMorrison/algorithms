c32bd809fee0eb36be3aa24c9c71ef4817cad6d7
Angus Morrison <contact@angus-morrison.com>
2020-02-08 15:22:24 +0000

    Insertion sort

--- c/sorting/insertion_sort.c
+++ c/sorting/insertion_sort.c
@@ -8,7 +8,6 @@
 */
 
 void sort(int arr[], size_t size);
-void swap(int* first, int* second);
 void print(int arr[], size_t size);
 
 int main()
@@ -17,4 +16,28 @@ int main()
   size_t size = sizeof(numbers) / sizeof(int);
   sort(numbers, size);
   print(numbers, size);
+}
+
+void sort(int arr[], size_t size)
+{
+  for (int i = 1; i < size; i++)
+  {
+    int key = arr[i];
+    int j = i - 1;
+    while (j >= 0 && arr[j] > key)
+    {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+}
+
+void print(int arr[], size_t size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%i ", arr[i]);
+    }
+    printf("\n");
 }
