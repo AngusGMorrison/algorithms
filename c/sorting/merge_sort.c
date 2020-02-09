91f07658f4e9c260c0534c5724e40c32adedbd43
Angus Morrison <contact@angus-morrison.com>
2020-02-09 14:07:31 +0000

    Improved C merge_sort implementation

--- c/sorting/merge_sort.c
+++ c/sorting/merge_sort.c
@@ -1,92 +1,72 @@
-#include<stdlib.h>
 #include<stdio.h>
 
-/*
-  Merge sort
-  - O(nlogn)
-  - Divide and conquer algorithm
-  - More efficient than quicksort for larger datasets
-  - Requires additional storage for auxiliary arrays
-  - Stable
-*/
+/**
+ * Merge sort
+ * - O(nlogn)
+ * - Divide and conquer algorithm
+ * - Outperforms n^2 sorts for all but very small datasets
+ * - Requires additional storage for helper array
+ * - Stable
+**/
 
-void sort(int arr[], int start_idx, int end_idx);
-void merge(int arr[], int start_idx, int mid_idx, int end_idx);
+void merge_sort(int arr[], size_t size);
+void split_merge(int arr[], int i_start, int i_end, int helper[]);
+void merge(int arr[], int i_start, int i_mid, int i_end, int helper[]);
 void print(int arr[], size_t size);
 
-int main()
-{
-    int numbers[] = { 4, 2, 7, 3, 5, 6, 0 };
-    size_t size = sizeof(numbers) / sizeof(int);
-    sort(numbers, 0, size - 1);
-    print(numbers, size);
+int main() {
+     int arr[] = { 4, 2, 7, 3, 5, 6, 0, 1 };
+     size_t size = sizeof(arr) / sizeof(arr[0]);
+     merge_sort(arr, size);
+     print(arr, size);
 }
 
-void sort(int arr[], int start_idx, int end_idx)
+void merge_sort(int arr[], size_t size)
 {
-  if (start_idx < end_idx)
-  {
-    int mid_idx = (start_idx + end_idx) / 2;
-    // Recursively sort both halves
-    sort(arr, start_idx, mid_idx);
-    sort(arr, mid_idx + 1, end_idx);
-    // Merge the sorted halves
-    merge(arr, start_idx, mid_idx, end_idx);
-  }
+    int helper[size];
+    split_merge(arr, 0, size - 1, helper);
 }
 
-void merge(int arr[], int start_idx, int mid_idx, int end_idx)
+void split_merge(int arr[], int i_start, int i_end, int helper[])
 {
-  // Create temporary arrays for to hold the two havles
-  int first_half_len = mid_idx - start_idx + 1;
-  int second_half_len = end_idx - mid_idx;
-
-  int first_half[first_half_len], second_half[second_half_len];
-
-  for (int i = 0; i < first_half_len; i++)
-  {
-    first_half[i] = arr[start_idx + i];
-  }
-
-  for (int i = 0; i < second_half_len; i++)
-  {
-    second_half[i] = arr[mid_idx + 1 + i];
-  }
-
-  // Merge the temporary arrays back into the original
-  int first_half_idx = 0;
-  int second_half_idx = 0;
-  int original_arr_idx = start_idx;
-
-  while (first_half_idx < first_half_len && second_half_idx < second_half_len)
-  {
-    if (first_half[first_half_idx] <= second_half[second_half_idx])
+    if (i_end <= i_start)
     {
-      arr[original_arr_idx] = first_half[first_half_idx];
-      first_half_idx++;
+        return;     // Array size is 1, don't split any further
     }
-    else
+    int i_mid = (i_end + i_start) / 2;              // Left subarray is arr[i_start..mid]
+                                                    // Right subarray is arr[(mid + 1)..i_end]
+    split_merge(arr, i_start, i_mid, helper);       // Sort the left subarray recursively
+    split_merge(arr, i_mid + 1, i_end, helper);     // Sort the right subarry recursively
+    // Merge the sorted subarrays
+    merge(arr, i_start, i_mid, i_end, helper);
+}
+
+void merge(int arr[], int i_start, int i_mid, int i_end, int helper[])
+{
+    int i_left_half = i_start;        // Counter to track position in the left subarray
+    int i_right_half = i_mid + 1;     // Counter to track position in the right subarray
+    
+    for (int k = i_start; k <= i_end; k++)   // k represents position in the original array
     {
-      arr[original_arr_idx] = second_half[second_half_idx];
-      second_half_idx++;
+        /* If there are items in the left half,
+           and the right half is empty
+           or the current item in the left half is <= the current item in the right half,
+           add it to the helper array. */
+        if (i_left_half <= i_mid && (i_right_half > i_end || arr[i_left_half] <= arr[i_right_half]))
+        {
+            helper[k] = arr[i_left_half];
+            i_left_half++;
+        }
+        else
+        {
+            helper[k] = arr[i_right_half];
+            i_right_half++;
+        }
     }
-    original_arr_idx++;
-  }
-
-  //Copy any remaining elements of either half
-  while (first_half_idx < first_half_len)
-  {
-    arr[original_arr_idx] = first_half[first_half_idx];
-    first_half_idx++;
-    original_arr_idx++;
-  }
-
-  while (second_half_idx < second_half_len)
-  {
-    arr[original_arr_idx] = second_half[second_half_idx];
-    second_half_idx++;
-    original_arr_idx++;
-  }
+    // Copy the sorted helper array to the original
+    for (int k = i_start; k <= i_end; k++) {
+        arr[k] = helper[k];
+    }
 }
 
 void print(int arr[], size_t size)
